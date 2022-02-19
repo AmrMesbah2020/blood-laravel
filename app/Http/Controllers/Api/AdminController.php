83dd5270e0cb18b82bae8aef1b4ae1997fec17ec
mariamRaslan <mariamraslan@gmail.com>
2022-02-19 20:07:27 +0200

    nnnn

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -62,7 +62,7 @@ class AdminController extends Controller
     }
 
     public function feedback(Request $request){
-
+        
         $input = $request->all();
 
         Feedback::create($input);
