d8bb1af8220330e9937042fad950229e5e8b4dc9
SamarMohamed1 <samar.mohamed.hassan@gmail.com>
2022-03-07 00:09:40 +0200

    image validation

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -26,6 +26,7 @@ class PostRequest extends FormRequest
         return [
             'title'=>'required|max:40|min:10',
             'content'=>'required|min:20|max:300',
+            'image'=>'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ];
     }
 
@@ -37,6 +38,7 @@ class PostRequest extends FormRequest
             'title.max' => 'for title max input must be at least 40 chars.',
             'content.required'=>' content field is required.',
             'content.max'=>'for content max input 200 chars.',
+            'image.max'=>'the max size of image is 2MB'
 
         ];
     }
